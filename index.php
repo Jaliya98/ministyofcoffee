f11302aad1f8d7140203d97c8afab688e3afa550
Jaliya Madusnka <rmjmbandara@students.nsbm.ac.lk>
2022-09-03 19:14:32 +0530

    images to image

--- index.php
+++ index.php
@@ -114,7 +114,7 @@
     <div class="row">
 
         <div class="image">
-            <img src="Gallery/images/images-20.jpeg" alt="">
+            <img src="Gallery/images/image-20.jpeg" alt="">
         </div>
 
         <div class="content">
